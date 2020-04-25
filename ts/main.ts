a27fd9da4abf1a1b1c691fa8ff4eb35dbde46bfb
Dillon Stricklett <dillonstricklett@live.com>
2020-04-25 00:56:25 -0700

    Added DOB validation

--- ts/main.ts
+++ ts/main.ts
@@ -7,6 +7,18 @@ function main():void{
     resetErrorMessages();
     isTextPresent("first-name", "First name is required");
     isTextPresent("last-name", "Last name is required");
+    // validate date
+    let dobBox = <HTMLInputElement>document.getElementById("dob");
+    let dob = dobBox.value;
+    if(!isValidDate(dob)){
+        dobBox.nextElementSibling.innerHTML = "Format should be mm/dd/yyyy"
+    }
+}
+
+function isValidDate(input:string):boolean{
+    // Validating mm/dd/yyyy
+    let pattern = /^\d{1,2}\/\d{1,2}\/\d{4}$/g
+    return pattern.test(input);
 }
 
 /**
